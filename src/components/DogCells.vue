74fb952b324131e40292d4c678a191232911fedd
Konstantin Zaytsev <zaitsumi@gmail.com>
2020-06-05 12:47:10 +0300

    fix: some fixes

--- src/components/DogCells.vue
+++ src/components/DogCells.vue
@@ -1,12 +1,19 @@
 <template>
-    <div class="grid">
-      <div v-for="(url, i) in list" :key="i" :dog="url" class="grid__item" :class="{'first': $route.name === 'Home' && i === 0}" :style="{'background-image': `url('${url}')`}">
-          <div class="grid__item__container">
-              <FavouriteButton :dog="url" />
-              <div class="grid__item__container__breed">{{ getBreedFromUrl(url) }}</div>
-          </div>
+  <div class="grid">
+    <div
+      v-for="(url, i) in list"
+      :key="i"
+      :dog="url"
+      class="grid__item"
+      :class="{'first': $route.name === 'Home' && i === 0}"
+      :style="{'background-image': `url('${url}')`}"
+    >
+      <div class="grid__item__container">
+        <FavouriteButton :dog="url" />
+        <div class="grid__item__container__breed">{{ getBreedFromUrl(url) }}</div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -37,44 +44,47 @@ export default {
 
 <style lang="scss">
 .grid {
-    display: flex;
-    flex-wrap: wrap;
-    margin-left: -30px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -30px;
 
-    &__item {
-        flex-grow: 1;
-        position: relative;
-        height: 290px;
-        width: calc(100% / 3 - 30px);
-        margin-bottom: 30px;
-        margin-left: 30px;
-        background: {
-            size: cover;
-        }
-        border-radius: 8px;
+  &__item {
+    position: relative;
+    height: 290px;
+    width: calc(100% / 3 - 30px);
+    margin-bottom: 30px;
+    margin-left: 30px;
+    background: {
+      size: cover;
+    }
+    border-radius: 8px;
 
-        &.first {
-          height: 500px;
-          width: 100%;
-        }
+    &.first {
+      height: 500px;
+      width: 100%;
+    }
 
-        &__container {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: linear-gradient(1deg, #000000 -5.53%, rgba(0, 0, 0, 0) 54.45%);
+    &__container {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(
+        1deg,
+        #000000 -5.53%,
+        rgba(0, 0, 0, 0) 54.45%
+      );
 
-            &__breed {
-                position: absolute;
-                font-weight: bold;
-                font-size: 25px;
-                right: 25px;
-                bottom: 25px;
-                text-transform: capitalize;
-            }
-        }
+      &__breed {
+        position: absolute;
+        font-weight: bold;
+        font-size: 25px;
+        right: 25px;
+        bottom: 25px;
+        text-transform: capitalize;
+      }
     }
   }
+}
 </style>
--- src/components/FavouriteButton.vue
+++ src/components/FavouriteButton.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="favourite-heart" :class="{'active': isActive}" @click="toggleFavourite"></div>
+  <div
+    class="favourite-heart"
+    :class="{'active': isActive}"
+    @click="toggleFavourite"
+  ></div>
 </template>
 
 <script>
@@ -36,21 +40,21 @@ export default {
 </script>
 
 <style lang="scss">
-    .favourite-heart {
-        position: absolute;
-        display: block;
-        top: 25px;
-        left: 25px;
-        width: 29px;
-        height: 26px;
-        cursor: pointer;
-        background: {
-            image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyOSAyNiI+PHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMy43NTggMjUuN0wyLjYyNSAxNC44NzNhOC41NTUgOC41NTUgMCAwMTAtMTIuMzMxYzMuMjQ5LTMuMTYgOC4zNy0zLjM3NSAxMS44NzUtLjY0NyAzLjUwNC0yLjcyOCA4LjYyNi0yLjUxMiAxMS44NzUuNjQ3YTguNTU1IDguNTU1IDAgMDEwIDEyLjMzMUwxNS4yNDIgMjUuN2ExLjA2NiAxLjA2NiAwIDAxLTEuNDg0IDB6TTQuMTA5IDQuMDVhNi40NTYgNi40NTYgMCAwMDAgOS4zMThMMTQuNSAyMy40N2wxMC4zOTEtMTAuMTAzYTYuNDU2IDYuNDU2IDAgMDAwLTkuMzE5Yy0yLjY2Mi0yLjU4OC02Ljk4Ny0yLjU4OC05LjY0OSAwYTEuMDY2IDEuMDY2IDAgMDEtMS40ODQgMGMtMi42NjItMi41ODgtNi45ODctMi41ODgtOS42NDkgMHoiIGZpbGw9IiNmZmYiLz48L3N2Zz4=);
-            size: contain;
-        }
+.favourite-heart {
+  position: absolute;
+  display: block;
+  top: 25px;
+  left: 25px;
+  width: 29px;
+  height: 26px;
+  cursor: pointer;
+  background: {
+    image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyOSAyNiI+PHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMy43NTggMjUuN0wyLjYyNSAxNC44NzNhOC41NTUgOC41NTUgMCAwMTAtMTIuMzMxYzMuMjQ5LTMuMTYgOC4zNy0zLjM3NSAxMS44NzUtLjY0NyAzLjUwNC0yLjcyOCA4LjYyNi0yLjUxMiAxMS44NzUuNjQ3YTguNTU1IDguNTU1IDAgMDEwIDEyLjMzMUwxNS4yNDIgMjUuN2ExLjA2NiAxLjA2NiAwIDAxLTEuNDg0IDB6TTQuMTA5IDQuMDVhNi40NTYgNi40NTYgMCAwMDAgOS4zMThMMTQuNSAyMy40N2wxMC4zOTEtMTAuMTAzYTYuNDU2IDYuNDU2IDAgMDAwLTkuMzE5Yy0yLjY2Mi0yLjU4OC02Ljk4Ny0yLjU4OC05LjY0OSAwYTEuMDY2IDEuMDY2IDAgMDEtMS40ODQgMGMtMi42NjItMi41ODgtNi45ODctMi41ODgtOS42NDkgMHoiIGZpbGw9IiNmZmYiLz48L3N2Zz4=);
+    size: contain;
+  }
 
-        &.active {
-            background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyOSAyNiI+PHBhdGggZD0iTTEzLjc1OCAyNS43Yy40MTIuNCAxLjA3Mi40IDEuNDg0IDBsMTEuMTMzLTEwLjgyNmE4LjU1NSA4LjU1NSAwIDAwMC0xMi4zMzFjLTMuMjQ5LTMuMTYtOC4zNy0zLjM3NS0xMS44NzUtLjY0Ny0zLjUwNC0yLjcyOC04LjYyNi0yLjUxMi0xMS44NzUuNjQ3YTguNTU1IDguNTU1IDAgMDAwIDEyLjMzMUwxMy43NTggMjUuN3oiIGZpbGw9IiNmZmYiLz48L3N2Zz4=);
-        }
-    }
+  &.active {
+    background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyOSAyNiI+PHBhdGggZD0iTTEzLjc1OCAyNS43Yy40MTIuNCAxLjA3Mi40IDEuNDg0IDBsMTEuMTMzLTEwLjgyNmE4LjU1NSA4LjU1NSAwIDAwMC0xMi4zMzFjLTMuMjQ5LTMuMTYtOC4zNy0zLjM3NS0xMS44NzUtLjY0Ny0zLjUwNC0yLjcyOC04LjYyNi0yLjUxMi0xMS44NzUuNjQ3YTguNTU1IDguNTU1IDAgMDAwIDEyLjMzMUwxMy43NTggMjUuN3oiIGZpbGw9IiNmZmYiLz48L3N2Zz4=);
+  }
+}
 </style>
